12d2fab80c3a6beb112cb17dda22df130d47340b
Hororohoruru <juanjesustorre@gmail.com>
2024-03-12 10:14:03 +0100

    overwritten nav vi for caps lock

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -38,10 +38,10 @@
 #endif
 
 #define MIRYOKU_LAYER_BASE \
-    KC_Q,              KC_W,               KC_E,              KC_R,         KC_T,            KC_Y,             KC_U,              KC_I,             KC_O,           KC_P,              \
-    LGUI_T(KC_A),      LALT_T(KC_S),       LCTL_T(KC_D),      LSFT_T(KC_F), KC_G,            KC_H,             LSFT_T(KC_J),      LCTL_T(KC_K),     LALT_T(KC_L),   LGUI_T(ES_ACUT),   \
-    LT(U_BUTTON,KC_Z), ALGR_T(KC_X),       KC_C,              KC_V,         KC_B,            KC_N,             KC_M,              KC_COMM,          ALGR_T(KC_DOT), LT(U_BUTTON,ES_QUOT),\
-    U_NP,U_NP,         LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),            LT(U_SYM,KC_ENT), LT(U_NUM,KC_BSPC), LT(U_FUN,KC_DEL),                  U_NP, U_NP \
+    KC_Q,               KC_W,               KC_E,           KC_R,           KC_T,       KC_Y,   KC_U,           KC_I,           KC_O,           KC_P,              \
+    LGUI_T(KC_A),       LALT_T(KC_S),       LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,       KC_H,   LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(ES_ACUT),   \
+    LT(U_BUTTON,KC_Z),  ALGR_T(KC_X),       KC_C,           KC_V,           KC_B,       KC_N,   KC_M,           KC_COMM,        KC_DOT,         ES_QUOT,           \
+    U_NP, U_NP,         LT(U_MEDIA,KC_ESC), LT(U_NAV,KC_SPC),   LT(U_MOUSE,KC_TAB),     LT(U_SYM,KC_ENT),   LT(U_NUM,KC_BSPC),  LT(U_FUN,KC_DEL),   U_NP, U_NP     \
 
 #define MIRYOKU_LAYER_EXTRA \
     KC_Q,              KC_D,               KC_R,              KC_W,         KC_B,            KC_J,             KC_F,              KC_U,              KC_P,           ES_ACUT,           \
@@ -60,6 +60,12 @@
     KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,           U_NU,              \
     U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,           U_NU,              \
     U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,              U_NP              \
+
+#define MIRYOKU_LAYER_NAV \
+TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           KC_CAPS,           \
+U_NA,              KC_ALGR,           TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    U_NA,              KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            KC_INS,            \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_BSPC,           KC_DEL,            U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
     KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,           U_NA,              KC_RALT,           TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
